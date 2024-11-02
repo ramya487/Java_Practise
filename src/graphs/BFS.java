fa0db1e79c31701b2216b80ebbbde6cbc722c7b3
ramya <ramyas.aiml2022@citchennai.net>
2024-11-02 10:20:49 +0530

    graph bfs

---
+++ src/graphs/BFS.java
@@ -0,0 +1,30 @@
+package graphs;
+import java.util.*;
+import graphs.ExampleGraph;
+
+public class BFS {
+    public static void main(String[] args){
+        List<List<Integer>> adjList = ExampleGraph.getAdjList(); // undirected non-weighted graph v: 1-8
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] vis = new int[adjList.size()];
+
+        List<Integer> bfs = new ArrayList<>();
+
+        q.add(1); // since range of vertices is from 1-8
+        vis[1] = 1;
+
+        while (!q.isEmpty()){
+            int front = q.poll();
+            bfs.add(front);
+            for (int neighbour: adjList.get(front)){
+                if (vis[neighbour] == 0){
+                    q.add(neighbour);
+                    vis[neighbour] = 1;
+                }
+            }
+        }
+
+        System.out.println(bfs);
+    }
+}
---
+++ src/graphs/ExampleGraph.java
@@ -0,0 +1,21 @@
+package graphs;
+import java.util.*;
+
+public class ExampleGraph {
+    public static List<List<Integer>> getAdjList(){
+        int v = 8;
+        int[][] edges = {{1,2}, {1,6}, {2,3}, {3,4}, {6,7},{6,8},{4,5},{7,5}};
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for (int i = 0; i<=v; i++){
+            adjList.add(new ArrayList<Integer>());
+        }
+
+        for (int[] edge: edges){
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]); // undirected non-weighted graph
+        }
+
+        return adjList;
+    }
+}
